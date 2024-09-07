599863d83dae9f35beb6f619245f0abd9e287a38
Gabriel Lamon <lamongabriel@icloud.com>
2024-09-07 17:23:47 -0300

    dimmed newsletter bg

--- frontend/src/components/marketing/navigation/footer/newsletter.tsx
+++ frontend/src/components/marketing/navigation/footer/newsletter.tsx
@@ -3,7 +3,7 @@ import { Input } from '@/components/ui/input'
 
 export function MarketingFooterNewsletter() {
   return (
-    <div className="bg-foreground py-16 sm:py-24 lg:py-32">
+    <div className="bg-foreground/90 py-16 sm:py-24 lg:py-32">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
         <div className="max-w-xl text-3xl font-bold tracking-tight text-white sm:text-4xl lg:col-span-7">
           <h2 className="inline sm:block lg:inline xl:block">
