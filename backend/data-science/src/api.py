e89c77c0fd9d3cd3bef3cf8dc163a5c0b93b11f4
Lamadelrae <matthew.almeida572@gmail.com>
2024-09-12 11:50:16 -0300

    fix: code style

--- backend/data-science/src/api.py
+++ backend/data-science/src/api.py
@@ -1,9 +1,19 @@
-from flask import Flask
-from controllers.PredictionController import bp as routes_bp
+from flask import Flask, Blueprint, jsonify, request
+from handlers.PredictionHandler import predict
 
+bp = Blueprint('routes', __name__)
+
+@bp.route('/prediction', methods=['POST'])
+def get_predictions():
+    data = request.get_json()
+    if data:
+        predictions = predict(data)
+        return jsonify(predictions), 200
+    else:
+        return jsonify({"error": "No JSON received"}), 400
+    
 api = Flask(__name__)
-
-api.register_blueprint(routes_bp)
+api.register_blueprint(bp)
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=5000, debug=True)
